3e574a8cf35a284cc0e1e2cc1f66cf538ad02273
Tom Wilshere <tom.wilshere@gmail.com>
2012-10-16 16:38:04 +0100

    Added google finance chart sample... Not sure I like it though.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -68,6 +68,27 @@ class SiteController < ApplicationController
     @piechart = GoogleVisualr::Interactive::PieChart.new(data_table, option)
     @barchart = GoogleVisualr::Interactive::BarChart.new(data_table, option)
 
+
+    data_table2 = GoogleVisualr::DataTable.new
+    data_table2.new_column('date'  , 'Date')
+    data_table2.new_column('number', 'Sold Pencils')
+    data_table2.new_column('string', 'title1')
+    data_table2.new_column('string', 'text1' )
+    data_table2.new_column('number', 'Sold Pens'   )
+    data_table2.new_column('string', 'title2')
+    data_table2.new_column('string', 'text2' )
+    data_table2.add_rows( [
+      [ Date.parse("2008-2-1"), 30000, '', '', 40645, '', ''],
+      [ Date.parse("2008-2-2"), 14045, '', '', 20374, '', ''],
+      [ Date.parse("2008-2-3"), 55022, '', '', 50766, '', ''],
+      [ Date.parse("2008-2-4"), 75284, '', '', 14334, 'Out of Stock','Ran out of stock on pens at 4pm'],
+      [ Date.parse("2008-2-5"), 41476, 'Bought Pens','Bought 200k pens', 66467, '', ''],
+      [ Date.parse("2008-2-6"), 33322, '', '', 39463, '', '']
+    ] )
+
+    opts   = { :displayAnnotations => true }
+    @timechart = GoogleVisualr::Interactive::AnnotatedTimeLine.new(data_table2, opts)
+
   end
 
 end
