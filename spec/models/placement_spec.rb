e83a82e3aa31e29fc2922cc3705e7a1117726946
Tom Wilding <tomwilding72@gmail.com>
2012-10-17 11:22:47 +0100

    Added tests to placement_spec.rb

--- spec/models/placement_spec.rb
+++ spec/models/placement_spec.rb
@@ -1,5 +1,48 @@
 require 'spec_helper'
 
 describe Placement do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @placement = FactoryGirl.create :placement
+  end
+
+  it "has a valid factory" do
+    @company.should be_valid
+  end
+  
+  it "requires a position" do
+    @placement.position = ""
+    @placement.should be_invalid
+  end
+
+  it "requires a description" do
+    @placement.description = ""
+    assert @placement.should be_invalid
+  end
+
+  it "requires a duration" do
+    @placement.duration = ""
+    @placement.should be_invalid
+  end
+
+  it "requires a location" do
+    @placement.location = ""
+    @placement.should be_invalid
+  end
+
+  it "requires a deadline" do
+    @placement.deadline = ""
+    assert @placement.should be_invalid
+  end
+
+  it "requires that a description contains less than 80 words" do
+    @placement.description = "word " * 79
+    @placement.should be_valid
+
+    @placement.description = "word " * 80
+    @placement.should be_valid
+
+    @placement.description = "word " * 81
+    @placement.should be_invalid
+  end
+
 end
