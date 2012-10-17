076e8bbee0873bbb8c100cdca97e1389cb31c5ad
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-10-17 16:19:03 +0100

    Validated deadline to be after the current date

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -1,9 +1,9 @@
 class Placement < ActiveRecord::Base
   belongs_to :company
 
-  validates :company,     :presence => true
-  validates :position,    :presence => true
-  validates :location,    :presence => true
-  validates :deadline,    :presence => true
-  validates :description, :presence => true
+  validates :company,           :presence => true
+  validates :position,          :presence => true
+  validates :location,          :presence => true
+  validates :description,       :presence => true
+  validates_datetime :deadline, :after => :today
 end
