345c6f6cadd482aa5a2783551210dd9308a953cc
Peter Hamilton <peter@inspiredpixel.net>
2012-10-18 13:41:51 +0100

    Added some sample Company seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Sample Users
 team = %w( peter tom jack sarah )
-
 team.each do |team_member|
   User.create([{email: "#{team_member}@cpp.com", :password => "cpp", :password_confirmation => "cpp"}])
 end
+
+# Sample Companies
+companies = Company.create([
+  { :name => "Google", :logo => "google.jpg", :description => "Google ins't evil" },
+  { :name => "Facebook", :logo => "facebook.jpg", :description => "Facebook move fast and break things" },
+  { :name => "Amazon", :logo => "amazon.jpg", :description => "World's most trusted company" }
+])
