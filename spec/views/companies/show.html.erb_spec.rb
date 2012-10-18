06533090eb540669945efbc924c80df90f57d6f9
Peter Hamilton <peter@inspiredpixel.net>
2012-10-18 13:30:00 +0100

    Generated basic controller and views for Company model

---
+++ spec/views/companies/show.html.erb_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "companies/show" do
+  before(:each) do
+    @company = assign(:company, stub_model(Company,
+      :name => "Name",
+      :logo => "Logo",
+      :description => "MyText"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Name/)
+    rendered.should match(/Logo/)
+    rendered.should match(/MyText/)
+  end
+end
