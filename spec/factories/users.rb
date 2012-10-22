0b9a176ef82d49e4d8b3dbb3f33fdef67f508103
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-10-22 16:06:28 +0100

    Removed users facotry as don't use it yet, can add it back in if we need to.

--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :user do
-    email Faker::Internet.email
-    password_digest Faker::Lorem.words(2).join
-  end
-end
