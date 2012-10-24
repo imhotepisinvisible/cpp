923cd759fca6d5adbcfd711cc1d0b87fd08023de
Peter Hamilton <peter@inspiredpixel.net>
2012-10-24 14:08:49 +0100

    Avoids duplicate event models by using proc

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,14 +3,14 @@
 FactoryGirl.define do
   factory :event do
     company
-    title Faker::Company.bs.truncate(20)
+    title { Faker::Company.bs.truncate(20) }
     start_date { FactoryHelper.time_rand }
     end_date { FactoryHelper.time_rand(start_date, start_date + 2.day.to_i) }
     deadline { FactoryHelper.time_rand(Time.now, start_date) }
-    description Faker::Company.bs
-    location Faker::Address.street_address
-    capacity Random.rand(100)
-    google_map_url Faker::Internet.url
+    description { Faker::Company.bs }
+    location { Faker::Address.street_address }
+    capacity { Random.rand(100) }
+    google_map_url { Faker::Internet.url }
   end
 end
 
