9ecf23ce808a609e5c064d1138e156f0ba52f9c7
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-10-26 20:07:55 +0100

    Removed commented out type validation in user, rails does it automatically, don't need to verify

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,6 @@ class User < ActiveRecord::Base
   validates :email,           :presence => true
   validates :password_digest, :presence => true, :on => :create
 
-  # validates :type, :inclusion => { :in => %w(Student),
-  #   :message => "%{type} is not a valid user type" }
-
   validates :password, :length =>{
     :minimum => 8,
     :too_short => "password is too short, must be at least %{count} characters"
