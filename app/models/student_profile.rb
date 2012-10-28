84dcc13e5acf939f7102914f184f6b685d4d6e97
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-10-28 16:38:58 +0000

    Removed tight correct year/degree validations. Trusting user

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -17,24 +17,4 @@ class StudentProfile < ActiveRecord::Base
     :too_long  => "must have at most %{count} characters"
   }
 
-  validate :year_must_be_valid_for_degree
-
-  # TOOD: How can we get degree only degrees?
-  def year_must_be_valid_for_degree
-    range = nil
-    case degree
-    when "MEng", "PhD"
-      range = 1..4
-    when "BEng"
-      range = 1..3
-    when "Msc"
-      range = 1..1
-    else
-      errors.add(:degree, "#{degree} isn't a valid degree type")
-    end
-
-    if range and !range.cover?(year)
-      errors.add(:year, "#{year} is not valid for degree type #{degree}")
-    end
-  end
 end
--- spec/models/student_profile_spec.rb
+++ spec/models/student_profile_spec.rb
@@ -27,26 +27,4 @@ describe StudentProfile do
     student_profile = FactoryGirl.build(:student_profile, :beng_student, year: 5)
     it {should be_invalid}
   end
-
-  context "when assigning students with a year" do
-    full_range = 1..4
-
-    {:beng_student => 1..3,
-     :meng_student => full_range,
-     :phd_student  => full_range,
-     :msc_student  => 1..1
-    }.each do |degree, range|
-      student_profile = FactoryGirl.build(:student_profile, degree)
-      range.each do |year|
-        it "#{degree} should be valid with year #{year}" do
-         student_profile.should be_valid_for_given_field(:year, year)
-        end
-      end
-      (full_range.to_a - range.to_a).each do |year|
-        it "#{degree} should not be valid with year #{year}" do
-          student_profile.should be_invalid_for_given_field(:year, year)
-        end
-      end
-    end
-  end
 end
