ba27d51c0b2eb5c957fc69bca5d58c3c5b695c9c
Peter Hamilton <peter@inspiredpixel.net>
2012-10-28 15:55:17 +0000

    Domain should validate organisation, not id

--- app/models/organisation_domain.rb
+++ app/models/organisation_domain.rb
@@ -1,7 +1,7 @@
 class OrganisationDomain < ActiveRecord::Base
   belongs_to :organisation
 
-  validates :organisation_id, :presence => true
+  validates :organisation, :presence => true
   validates :domain, :presence => true
 
 end
