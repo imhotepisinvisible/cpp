be7ac2508992f37047c3793a1997a94bea7a1f2f
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-10-28 16:08:50 +0000

    Changed user tests to ensure it behaves like a user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  before(:each) do
-    @user = FactoryGirl.build :user
-  end
-
-  it "has a valid factory" do
-    @user.should be_valid
-  end
-
-  it "user model has secure password" do 
-    @user.should respond_to(:password_digest)
-    @user.should respond_to(:authenticate)
-  end
-
-  it "requires an email address" do
-    @user.email = ""
-    @user.should be_invalid
-  end
-
-  it "requires a password digest" do
-    @user.password_digest = ""
-    @user.should be_invalid
+  it_should_behave_like "a user" do
+    let(:unsaved) { FactoryGirl.build :user }
+    let(:saved) { FactoryGirl.create :user }
   end
 end
