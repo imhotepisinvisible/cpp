43a95c91ca96d8dbe607fb96131d90d0fc36d9ac
Peter Hamilton <peter@inspiredpixel.net>
2012-10-28 15:56:13 +0000

    Corrected variable names in student_profile spec

--- spec/models/student_profile_spec.rb
+++ spec/models/student_profile_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe StudentProfile do
   include AttributeValidators
-  context "when validating any student" do
+  context "when validating any student profile" do
     subject { FactoryGirl.build(:student_profile,
                                    :beng_student,
                                    :first_year) }
+    
     it {should be_valid}
 
     context "when attributes are not set" do
@@ -23,7 +24,7 @@ describe StudentProfile do
   end
 
   context "when assigning an invalid year year" do
-    student = FactoryGirl.build(:student_profile, :beng_student, year: 5)
+    student_profile = FactoryGirl.build(:student_profile, :beng_student, year: 5)
     it {should be_invalid}
   end
 
@@ -35,15 +36,15 @@ describe StudentProfile do
      :phd_student  => full_range,
      :msc_student  => 1..1
     }.each do |degree, range|
-      student = FactoryGirl.build(:student_profile, degree)
+      student_profile = FactoryGirl.build(:student_profile, degree)
       range.each do |year|
         it "#{degree} should be valid with year #{year}" do
-         student.should be_valid_for_given_field(:year, year)
+         student_profile.should be_valid_for_given_field(:year, year)
         end
       end
       (full_range.to_a - range.to_a).each do |year|
         it "#{degree} should not be valid with year #{year}" do
-          student.should be_invalid_for_given_field(:year, year)
+          student_profile.should be_invalid_for_given_field(:year, year)
         end
       end
     end
