795c12ea8677620983157bd6007d0f724d198e47
Peter Hamilton <peter@inspiredpixel.net>
2012-10-30 22:33:38 +0000

    Altered events factory to give a longer description and a capitalized title

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,11 +3,11 @@
 FactoryGirl.define do
   factory :event do
     company
-    title { Faker::Company.bs.truncate(20) }
+    title { Faker::Company.bs.truncate(20).capitalize }
     start_date { FactoryHelper.time_rand }
     end_date { FactoryHelper.time_rand(start_date, start_date + 2.day.to_i) }
     deadline { FactoryHelper.time_rand(Time.now, start_date) }
-    description { Faker::Company.bs }
+    description { 50.times.map{|i| Faker::Company.bs }.join ' ' }
     location { Faker::Address.street_address }
     capacity { Random.rand(100) }
     google_map_url { Faker::Internet.url }
