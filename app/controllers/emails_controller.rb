f0618ce17b745f2b80a77dd6adbe297ef8821a78
Tom Wilshere <tw310@Toms-MacBook-Pro.local>
2012-11-03 19:51:51 +0000

    I may have forgotten to include these files... New files associated with email CRUD.

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,63 @@
+class EmailsController < ApplicationController
+  respond_to :json
+  
+  # GET /emails
+  # GET /emails.json
+  def index
+    @emails = Email.scoped
+
+    if params.keys.include? "company_id"
+      @emails = @emails.where(:company_id => params[:company_id])
+    end
+
+    if params.keys.include? "limit"
+      @emails = @emails.limit(params[:limit])
+    end
+
+    respond_with @emails
+  end
+
+  # GET /emails/1
+  # GET /emails/1.json
+  def show
+    @email = Email.find(params[:id])
+    respond_with @email
+  end
+
+  # GET /emails/new
+  # GET /emails/new.json
+  def new
+    @email = Email.new
+    respond_with @email
+  end
+
+  # POST /emails
+  # POST /emails.json
+  def create
+    @email = Email.new(params[:email])
+    if @email.save
+      respond_with @email, status: :created, location: @email
+    else
+      respond_with @email, status: :unprocessable_entity
+    end
+  end
+
+  # PUT /emails/1
+  # PUT /emails/1.json
+  def update
+    @email = Email.find(params[:id])
+    if @email.update_attributes(params[:email])
+      head :no_content
+    else
+      respond_with @email, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /emails/1
+  # DELETE /emails/1.json
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
+    head :no_content
+  end
+end
