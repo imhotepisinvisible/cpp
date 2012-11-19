2f5fc2626e6eb147c7a7227670600555fd44f0d0
Peter Hamilton <peter@inspiredpixel.net>
2012-11-19 19:05:01 +0000

    Replaced a hackathon-remnant variable with cpp

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-CPP::Application.config.session_store :cookie_store, key: '_hackathon_template_session'
+CPP::Application.config.session_store :cookie_store, key: '_cpp_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
