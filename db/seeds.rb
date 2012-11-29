34192c527107c28a592a1aa651de7b1e310e4034
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-11-29 21:40:51 +0000

    Display department notifications on placements, ref #104
    * Modified placment new router to fetch company and departments
    * Show notice in a div
    * Still need to work out how to get html links to appear
    * Probably should have some differentiator between multiple
      department notes so the company know which department theyre for

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,7 +39,7 @@ FactoryGirl.create :organisation_domain, {
 department = FactoryGirl.create :department, {
   name:"Department of Computing",
   organisation: organisation,
-  settings_notifier_placement: "If this is your first placement you're offering, make sure you send back a placement performa and a health and saftey form which can be found on https://www.doc.ic.ac.uk/internal/industrialplacements/employers/IPproforma12-studentorganised.doc",
+  settings_notifier_placement: "If this is your first placement you're offering, make sure you send back a placement performa and a health and saftey form which can be found on https://www.doc.ic.ac.uk/internal/industrialplacements/employers/IPproforma12-studentorganised.doc and https://www.doc.ic.ac.uk/internal/industrialplacements/employers/Placement_Provider_Information_Form_2012.doc",
   settings_notifier_event: "Please note that at current an event must be scheduled at least two weeks in advance to be approved."
 }
 
