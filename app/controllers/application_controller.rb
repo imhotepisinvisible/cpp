4af867c63b619937b11f6bd3f517b0f747ce77bf
Peter Hamilton <peter@inspiredpixel.net>
2012-12-17 17:50:33 +0000

    Cancan authorisation errors should use 401 status not 403 (forbidden)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # If any controllers raise authentication exceptions, catch them here and
   # respond to the client with a 403 (auth fail) + exception message in the body
   rescue_from CanCan::AccessDenied do |exception|
-    render :text => exception.message, :status => 403
+    render :text => exception.message, :status => 401
   end
 
   private
