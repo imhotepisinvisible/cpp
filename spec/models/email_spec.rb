eeefbad382c7d31ae876f3734970283325e0d8a7
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-12-29 08:42:50 +0000

    Add model tests for #106

--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -6,13 +6,16 @@ describe Email do
   it { should be_valid }
 
   it { should belong_to :company }
-  it { should validate_presence_of :company_id }
 
   context "when creating or saving" do
-    fields = [:subject, :body, :company_id]
+    fields = [:subject, :body, :company_id, :state]
     fields.each do |field|
       it { should validate_presence_of field }
     end
+    
+    it { should ensure_inclusion_of(:state).in_array(
+      ["Rejected", "Pending", "Approved", "Postponed"]
+    )}
   end
 
 end
