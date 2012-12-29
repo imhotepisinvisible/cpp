7a293be00125852963cc1efbe3f895df9c25137b
Peter Hamilton <peter@inspiredpixel.net>
2012-12-29 16:10:48 +0000

    Updated auth for DepartmentAdministrator

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -17,12 +17,22 @@ class Ability
         !(company_deps | student_deps).empty?
       end
     when "CompanyAdministrator"
+      can :manage, CompanyAdministrator, :id => user.id
       can :manage, Company, :id => user.company_id
       can [:read, :download_document], Student do |student|
         # Get departments for both and check they intersect
         company_deps = user.company.departments.map(&:id)
         student_deps = student.departments.map(&:id)
         !(company_deps | student_deps).empty?
+      end
+    when "DepartmentAdministrator"
+      can :manage, DepartmentAdministrator, :id => user.id
+      can :manage, Department, :id => user.department_id
+      can :manage, Company do |company|
+        company.departments.map(&:id).include? user.department_id
+      end
+      can :manage, Student do |student|
+        student.departments.map(&:id).include? user.department_id
       end
     end
     #
