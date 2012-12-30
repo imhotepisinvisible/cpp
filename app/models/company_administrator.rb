4a846c4cfe8628c3fbd4f963b2db5da3b14db781
Peter Hamilton <peter@inspiredpixel.net>
2012-12-30 04:48:07 +0000

    Shortcuts for events and placements for admins

--- app/models/company_administrator.rb
+++ app/models/company_administrator.rb
@@ -1,6 +1,9 @@
 class CompanyAdministrator < User
   belongs_to :company
 
+  has_many :events, :through => :company, :uniq => true
+  has_many :placements, :through => :company, :uniq => true
+
   attr_accessible :company_id
 
   def as_json(options={})
--- app/models/department_administrator.rb
+++ app/models/department_administrator.rb
@@ -1,6 +1,10 @@
 class DepartmentAdministrator < User
   belongs_to :department
 
+  has_many :companies, :through => :department, :uniq => true
+  has_many :events, :through => :companies, :uniq => true
+  has_many :placements, :through => :companies, :uniq => true
+
   attr_accessible :department_id
 
   def as_json(options={})
