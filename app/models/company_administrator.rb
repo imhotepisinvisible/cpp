67b153593d3dcee02db9275edef77fb0449f6115
Peter Hamilton <peter@inspiredpixel.net>
2012-12-30 01:49:54 +0000

    Company and Department admin json (+type) implementation

--- app/models/company_administrator.rb
+++ app/models/company_administrator.rb
@@ -2,4 +2,8 @@ class CompanyAdministrator < User
   belongs_to :company
 
   attr_accessible :company_id
+
+  def as_json(options={})
+    super(:methods => [:type])
+  end
 end
--- app/models/department_administrator.rb
+++ app/models/department_administrator.rb
@@ -2,4 +2,8 @@ class DepartmentAdministrator < User
   belongs_to :department
 
   attr_accessible :department_id
+
+  def as_json(options={})
+    super(:methods => [:type])
+  end
 end
