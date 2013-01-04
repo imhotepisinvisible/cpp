15be358f881908df6a9611430e1bf5ec4c8bb911
Peter Hamilton <peter@inspiredpixel.net>
2013-01-04 11:54:54 +0000

    Updated department/pending department validations
    Essentially this stipulates that departments are required unless there are pending departments
    Also, vice-versa, if there are no pending departments, we require at least one confirmed department
    
    It's an either-or style validation.

--- app/models/company.rb
+++ app/models/company.rb
@@ -37,10 +37,11 @@ class Company < ActiveRecord::Base
     :content_type => { :content_type => ["image/jpeg", "image/jpg", "image/png"],
                         message: "Must be a jpeg or png file"}
 
-  # validates :departments,     :presence => { :message => "Must belong to at least one department" }
-  validates :name,            :presence => true
-  validates :description,     :presence => true
-  validates :organisation_id, :presence => true
+  validates :pending_departments,     :presence => { :message => "Must belong to at least one department" }, :if => lambda { self.departments.empty? }
+  validates :departments,             :presence => { :message => "Must belong to at least one department" }, :if => lambda { self.pending_departments.empty? }
+  validates :name,                    :presence => true
+  validates :description,             :presence => true
+  validates :organisation_id,         :presence => true
 
   validates :name, obscenity: {message: "Profanity is not allowed!"}
   validates :description, obscenity: {message: "Profanity is not allowed!"}
