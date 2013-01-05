f58f3d24cce066620f3df01c22f56e789946fd1a
Tom Wilshere <tom.wilshere@gmail.com>
2013-01-05 16:36:18 +0000

    Added type field to email to fix relationship issue.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130103171932) do
+ActiveRecord::Schema.define(:version => 20130105163330) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(:version => 20130103171932) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "event_id"
+    t.string   "type"
   end
 
   create_table "emails_students", :force => true do |t|
