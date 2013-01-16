088bca6428adfd925da406b5c4aa09ac470869c8
Peter Hamilton <peter@inspiredpixel.net>
2013-01-16 05:41:23 +0000

    Commented up AuditItem model

--- app/models/audit_item.rb
+++ app/models/audit_item.rb
@@ -1,6 +1,13 @@
+# Audit items are used in the department administrator stats and
+# show a timeline of relevant events in the CPP application's history
 class AuditItem
   attr_accessor :model, :timestamp, :type, :message, :url
 
+  # model - the associated model
+  # timestamp - the timestamp for when the event occurred
+  # type - used on the front end to alter the visual appearance
+  # message - brief summary of item/change
+  # url - if the item could link to another page, put the url here
   def initialize(model, timestamp, type = "unknown", message = "", url = nil)
     @model = model
     @timestamp = timestamp
