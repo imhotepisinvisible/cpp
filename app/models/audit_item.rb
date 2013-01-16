7d01e6d16a327653e914aee2c26b8da3a7c3d471
Peter Hamilton <peter@inspiredpixel.net>
2013-01-16 05:18:20 +0000

    Removed unused method

--- app/models/audit_item.rb
+++ app/models/audit_item.rb
@@ -1,9 +1,5 @@
 class AuditItem
-
   attr_accessor :model, :timestamp, :type, :message, :url
-
-  def between_dates(start_date = 7.days.ago, end_date = Date.today)
-  end
 
   def initialize(model, timestamp, type = "unknown", message = "", url = nil)
     @model = model
