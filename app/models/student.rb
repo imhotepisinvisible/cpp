371ead5e26349a7afb1f7ccf725cc477c9186a1a
Tom Wilshere <tom.wilshere@gmail.com>
2014-08-06 13:02:14 +0100

    Hide password hashes sent in json. Fixes #24.

--- app/models/student.rb
+++ app/models/student.rb
@@ -123,7 +123,7 @@ class Student < User
 
   # Returns JSON object
   def as_json(options={})
-    result = super(:methods => [:skill_list, :interest_list, :year_group_list, :reject_skill_list, :reject_interest_list, :type])
+    result = super(:except => [:password_digest], :methods => [:skill_list, :interest_list, :year_group_list, :reject_skill_list, :reject_interest_list, :type])
     result[:stat_count] = @stat_count
     return result
   end
