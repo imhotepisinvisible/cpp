d509ef26c6464e50ad67b60e0905c56447f9a7a4
Oliver Grubin <oliver.grubin14@imperial.ac.uk>
2015-02-25 23:26:41 +0000

    Fixes courses edit view bug and a couple of other small changes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -13,7 +13,7 @@ class CoursesController < ApplicationController
   # GET /courses/1.json
   def show
     @course = Course.find(params[:id])
-    respond_with @courses
+    respond_with @course
   end
 
   # GET /courses/new
